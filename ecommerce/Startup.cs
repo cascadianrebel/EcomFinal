cdff20c26fce3ff70c755b0bbf185169af55417d
cascadianrebel <Anthonydgreen90@gmail.com>
2018-07-30 10:59:38 -0700

    switched to production

--- ecommerce/Startup.cs
+++ ecommerce/Startup.cs
@@ -22,10 +22,10 @@ namespace ecommerce
 
         public Startup(IConfiguration configuration)
         {
-            //var builder = new ConfigurationBuilder().AddEnvironmentVariables();
-            //builder.AddUserSecrets<Startup>();
-            //Configuration = builder.Build();
-            Configuration = configuration;
+            var builder = new ConfigurationBuilder().AddEnvironmentVariables();
+            builder.AddUserSecrets<Startup>();
+            Configuration = builder.Build();
+            //Configuration = configuration;
         }
 
         // This method gets called by the runtime. Use this method to add services to the container.
@@ -77,17 +77,17 @@ namespace ecommerce
             services.AddScoped<IEmailSender, EmailSender>();
             services.AddScoped<IOrder, DevOrder>();
 
-            services.AddDbContext<EcomDbContext>(options =>
-            options.UseSqlServer(Configuration["ConnectionStrings:DefaultConnection"]));
-
-            services.AddDbContext<ApplicationDbContext>(options =>
-               options.UseSqlServer(Configuration["ConnectionStrings:UserConnection"]));
+            //services.AddDbContext<EcomDbContext>(options =>
+            //options.UseSqlServer(Configuration["ConnectionStrings:DefaultConnection"]));
 
             //services.AddDbContext<ApplicationDbContext>(options =>
-            //    options.UseSqlServer(Configuration["ConnectionStrings:SquirrelConnection"]));
+            //   options.UseSqlServer(Configuration["ConnectionStrings:UserConnection"]));
 
-            //services.AddDbContext<EcomDbContext>(options =>
-            //    options.UseSqlServer(Configuration["ConnectionStrings:ProductionConnection"]));
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(Configuration["ConnectionStrings:SquirrelConnection"]));
+
+            services.AddDbContext<EcomDbContext>(options =>
+                options.UseSqlServer(Configuration["ConnectionStrings:ProductionConnection"]));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
