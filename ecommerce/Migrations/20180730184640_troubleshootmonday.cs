19b9ac24724cdea6c780d77634fb1ddaa83ef1ec
cascadianrebel <Anthonydgreen90@gmail.com>
2018-07-30 14:50:40 -0700

    changed startup to revert back to development

---
+++ ecommerce/Migrations/20180730184640_troubleshootmonday.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ecommerce.Migrations
+{
+    public partial class troubleshootmonday : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
