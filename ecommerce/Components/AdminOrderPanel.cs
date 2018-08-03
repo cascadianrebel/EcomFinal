abdf2d5e6a5daed8ad1c5a8a6919f235cdc7e04e
cascadianrebel <Anthonydgreen90@gmail.com>
2018-08-02 21:49:40 -0700

    added vulnerabilities ; removed some unused namespaces so i could copy and paste some stuff.

--- ecommerce/Components/AdminOrderPanel.cs
+++ ecommerce/Components/AdminOrderPanel.cs
@@ -1,11 +1,8 @@
 ﻿using ecommerce.Data;
 using ecommerce.Models;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
